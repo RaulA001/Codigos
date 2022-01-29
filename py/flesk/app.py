b77f95462ca2a7442036096d6d7a6658636e881b
RaulA001 <95509340+RaulA001@users.noreply.github.com>
2022-01-29 01:18:05 -0300

    a

--- py/flesk/app.py
+++ py/flesk/app.py
@@ -1,5 +1,23 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, redirect, url_for, request
+app = Flask(__name__, static_folder='miracica/')
+
+#lembra de voltar na aula 6
+
+#forma 1 de cria rota
+@app.route('/')
+def index():
+    return redirect(url_for('oi'))
+
+@app.route('/oi/')
+@app.route('/oi/<nome>', methods=['GET', 'POST'])
+def oi(nome=''):
+    res = f'<h1>oi {nome}</h1> {request.method}'
+    return res
+#forma 2 de cria rota
+
+def test():
+    return '<1>Testado2</h1>'
+app.add_url_rule('/test', 'test', test)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True, port='3000')
--- py/flesk/primeira/app/controlles/default.py
+++ py/flesk/primeira/app/controlles/default.py
@@ -3,4 +3,4 @@ from app import app
 
 @app.route('/')
 def index():
-    return 'alaud'
+    return '<div> <P> É água pra todo lado,<br>  É muito sapo a coaxar,<br>  Vegetação vestindo verde,<br>  É o inverno a chegar.<br>  É a esperança brotando,<br>  No chão do meu Ceará.<br>  </P> </div>'
