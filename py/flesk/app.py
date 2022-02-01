f98bc82819883f37ad40b4b6a2a4578df6e9f129
RaulA001 <95509340+RaulA001@users.noreply.github.com>
2022-02-01 00:15:59 -0300

    a

--- py/flesk/app.py
+++ py/flesk/app.py
@@ -1,23 +1,33 @@
-from flask import Flask, redirect, url_for, request
-app = Flask(__name__, static_folder='miracica/')
 
-#lembra de voltar na aula 6
+from flask import Flask, redirect, url_for, request, abort, render_template
+from json import dumps
+app = Flask(__name__, static_folder='miracica/', template_folder='template')
 
 #forma 1 de cria rota
 @app.route('/')
 def index():
-    return redirect(url_for('oi'))
+    #q2 não funciona
+    query2 = dumps(request.args)
+    query = request.args.to_dict()
+    print(query2, query)
 
-@app.route('/oi/')
-@app.route('/oi/<nome>', methods=['GET', 'POST'])
+    return render_template('index.html', x=10, y=101, query=query)
+
+@app.route('/oi/', methods=['GET', 'POST'])
+@app.route('/oi/<nome>')
 def oi(nome=''):
-    res = f'<h1>oi {nome}</h1> {request.method}'
-    return res
+    if True:
+        print(request.method, request.args)
+        res = f'<h1>oi {nome}</h1> <p>{dumps(request.form)} <br> {request.method}</p> <a href="http://127.0.0.1:5000/miracica/index.html">Voltar</a>'
+        return res
+    else:
+        abort(403)
 #forma 2 de cria rota
 
 def test():
-    return '<1>Testado2</h1>'
+    print(request.method, ' | ', request.args)
+    return f'<h1>Testado2</h1> <p>é {dumps(request.args)}<p>'
 app.add_url_rule('/test', 'test', test)
 
 if __name__ == '__main__':
-    app.run(debug=True, port='3000')
+    app.run(debug=True)
