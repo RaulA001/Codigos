27aef09e2347f355c7fdb95267b6c0503bb02a05
RaulA001 <95509340+RaulA001@users.noreply.github.com>
2022-02-02 00:52:50 -0300

    ç

--- py/flesk/app.py
+++ py/flesk/app.py
@@ -1,17 +1,38 @@
 
-from flask import Flask, redirect, url_for, request, abort, render_template
+from flask import Flask, redirect, url_for, request, abort, render_template, make_response
 from json import dumps
 app = Flask(__name__, static_folder='miracica/', template_folder='template')
 
 #forma 1 de cria rota
-@app.route('/')
+@app.route('/', methods=['GET' ,'POST'])
 def index():
     #q2 não funciona
     query2 = dumps(request.args)
     query = request.args.to_dict()
     print(query2, query)
 
-    return render_template('index.html', x=10, y=101, query=query)
+    res = make_response((render_template('index.html', x=10, y=101, query=query)))
+
+    return res
+
+    #return render_template('index.html', x=10, y=101, query=query)
+
+@app.route('/cal/', methods=['POST'])
+def cal():
+    nota = sum([int(v) for v in request.form.to_dict().values()])
+
+    res = make_response((render_template('cal.html', nota=nota)))
+    if request.method == 'POST':
+        dado = request.form['nota2']
+        res.set_cookie('nota2', dado)
+
+    print(request.method)
+    return res
+
+@app.route('/cok')
+def cok():
+    dados = request.cookies.get('nota2')
+    return f'{dados}'
 
 @app.route('/oi/', methods=['GET', 'POST'])
 @app.route('/oi/<nome>')
